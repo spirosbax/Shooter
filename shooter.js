482a25c990f70f63bd7e1192f1cb3113be7bcaa2
Spiros Baxevanakis <spiros.baxevanakis@gmail.com>
2018-10-30 19:52:05 +0200

    fix black screen on gh-pages

--- shooter.js
+++ shooter.js
@@ -40,10 +40,10 @@ function preload() {
     game.load.image('bullet', './assets/bullets/bullet.png');
     game.load.image('enemy2', './assets/enemies/enemy2.png');
     game.load.image('enemy3', './assets/enemies/enemy3.png');
-    game.load.image('enemy3Bullet', '/assets/bullets/blue-enemy-bullet.png');
+    game.load.image('enemy3Bullet', './assets/bullets/blue-enemy-bullet.png');
     game.load.spritesheet('explosion', './assets/explode.png', 128, 128);
 
-    game.load.bitmapFont('font', './assets/font/font.png', '/assets/font/font.xml');
+    game.load.bitmapFont('font', './assets/font/font.png', './assets/font/font.xml');
 
     game.load.audio('background', './assets/audio/Wice_StarFighter.mp3');
     game.load.audio('shoot', './assets/audio/EnemyShoot.wav');
